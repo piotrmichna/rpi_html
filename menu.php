3c16678c3c8e6aa45a43cd7fc0a99ab94c029665
Piotr Michna <pm@piotrmichna.pl>
2020-06-13 10:50:37 +0200

    dodanei odnosnika Aktualizacje w menu Ustawienia

--- menu.php
+++ menu.php
@@ -17,6 +17,7 @@ echo "\n";
 		echo '		<ul>' . "\n";
 		echo '			<li><a href="cnf_hist.php"><i class="demo-icon icon-th"></i>Historia-system</a></li>' . "\n";
 		echo '			<li><a href="cnf_hist_prog.php"><i class="demo-icon icon-th"></i>Historia-program</a></li>' . "\n";
+		echo '			<li><a href="cnf_update.php"><i class="demo-icon icon-th"></i>Aktualizacja</a></li>' . "\n";
 		echo '			<li><a href="cnf_restart.php"><i class="demo-icon icon-th"></i>Restart systemu</a></li>' . "\n";		
 		echo '			<li><a href="cnf_poweroff.php"><i class="demo-icon icon-fork"></i>Wyłącz system</a></li>' . "\n";
 		echo '		</ul>' . "\n";
