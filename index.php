acdfa433fce503fb50e0b2fd7904b50530a46e76
Piotr Michna <pm@piotrmichna.pl>
2020-06-18 21:58:55 +0200

    dostosowanie strony glownej

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title>IDR - logowanie</title>
 	<link rel="shortcut icon" type="image/x-icon" href="img/green.ico" />
-	
+
 	<link rel="stylesheet" href="style.css" type="text/css"/>
 	<link rel="stylesheet" href="style_form.css" type="text/css"/>
 	<link rel="stylesheet" href="css/fontello.css" type="text/css"/>
@@ -14,23 +14,23 @@
 </head>
 
 <body>
-	<div id="main"> 
+	<div id="main">
 		<div id="toolbar">
-			<a href="index.php"><div class="tyt"><i class="demo-icon icon-tencent-weibo"></i>IDR</div></a>
-			<div class="nav">		
+			<a href="index.php"><div class="tyt"><i class="demo-icon icon-tencent-weibo"></i>JAS</div></a>
+			<div class="nav">
 				<?php
 					require_once "menu.php";
 				?>
 			</div>
-			<div style="clear:both"></div>			
-		</div>		
+			<div style="clear:both"></div>
+		</div>
 		<div id="pgst">
 		</br>
-		<?php 
-			
+		<?php
+
 				//require_once "connect.php";
 				$con = mysqli_connect("localhost", "pituEl", "hi24biscus", "homster");
-				
+
 				if (mysqli_connect_errno())
 				{
 					echo "Error: ".mysqli_connect_errno();
@@ -40,8 +40,8 @@
 				{
 					$conw = mysqli_connect("localhost", "pituEl", "hi24biscus", "homsweather");
 					echo '<div class="dpList">';
-					
-					$wym=mysqli_query($conw, 'SELECT * FROM bme ORDER BY id;');	
+
+					$wym=mysqli_query($conw, 'SELECT * FROM bme ORDER BY id;');
 					$i=0;
 					while ( $wym_row=mysqli_fetch_assoc($wym) ){
 							if($wym_row['para']=="temp"){
@@ -71,10 +71,10 @@
 			?>
 		</div>
 		<div class="footer">
-			<?php	
-				echo '<div class="stPlay">2020</div> <div class="stStop">Copyright Piotr Michna</div>';				
-			?>		
-			<div style="clear:both"></div>	
+			<?php
+				echo '<div class="stPlay">2020</div> <div class="stStop">Copyright Piotr Michna</div>';
+			?>
+			<div style="clear:both"></div>
 		</div>
 	</div>
 </body>
--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@ echo "\n";
 		echo '		<ul>' . "\n";
 		echo '			<li><a href="cnf_hist.php"><i class="demo-icon icon-th"></i>Historia-system</a></li>' . "\n";
 		echo '			<li><a href="cnf_hist_prog.php"><i class="demo-icon icon-th"></i>Historia-program</a></li>' . "\n";
-		echo '			<li><a href="cnf_restart.php"><i class="demo-icon icon-th"></i>Restart systemu</a></li>' . "\n";		
+		echo '			<li><a href="cnf_restart.php"><i class="demo-icon icon-th"></i>Restart systemu</a></li>' . "\n";
 		echo '			<li><a href="cnf_poweroff.php"><i class="demo-icon icon-fork"></i>Wyłącz system</a></li>' . "\n";
 		echo '		</ul>' . "\n";
 		echo '	</li>' . "\n";
@@ -15,7 +15,7 @@ echo "\n";
 		echo '		<a href="#"><i class="demo-icon icon-share"></i>Ustawienia</a>' . "\n";
 		echo '		<ul>' . "\n";
 		echo '			<li><a href="#"><i class="demo-icon icon-th"></i>Cykle</a></li>' . "\n";
-		echo '			<li><a href="#"><i class="demo-icon icon-fork"></i>Wyjścia</a></li>' . "\n";
+		echo '			<li><a href="us_param.php"><i class="demo-icon icon-fork"></i>Parametry</a></li>' . "\n";
 		echo '			<li><a href="#"><i class="demo-icon icon-flow-parallel"></i>Wejścia</a></li>' . "\n";
 		echo '			<li><a href="#"><i class="demo-icon icon-flow-parallel"></i>Programy</a></li>' . "\n";
 		echo '			<li><a href="cnf_update.php"><i class="demo-icon icon-th"></i>Aktualizacja</a></li>' . "\n";
@@ -23,4 +23,4 @@ echo "\n";
 		echo '	</li>' . "\n";
 		echo '</ol>' . "\n";
 
-?>	
+?>
